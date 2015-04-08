44281413536394cd74a3e55f06fb9a3458a1f694
Tomas Karasek <tom.to.the.k@gmail.com>
2015-04-08 14:15:23 +0300

    :spaghetti: added OSX binaries

--- getcompose.sh
+++ getcompose.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 VERSION=1.1.0
+OS=darwin
+ARCH=x86_64
 
-curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-${OS}-${ARCH} > docker-compose
+
 chmod +x docker-compose

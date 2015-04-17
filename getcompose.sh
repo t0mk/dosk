8fdd2c6d7664f0f880a486045146d0c610b8ff97
Tomas Karasek <tom.to.the.k@gmail.com>
2015-04-17 14:12:50 +0300

    :id: bump compose version

--- getcompose.sh
+++ getcompose.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.1.0
+VERSION=1.2.0
 
 curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
